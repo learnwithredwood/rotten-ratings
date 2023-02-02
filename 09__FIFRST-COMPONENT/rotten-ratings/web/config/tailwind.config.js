59823c10848da59229e3e12db51aabe8aa1cfedf
Amy Dutton <amy@ahhacreative.com>
2023-02-02 11:41:33 -0600

    ✨ Finished Lesson 9, added our first component

--- 09__FIFRST-COMPONENT/rotten-ratings/web/config/tailwind.config.js
+++ 09__FIFRST-COMPONENT/rotten-ratings/web/config/tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         almostBlack: '#242424',
-        lemonLime: '#E6FF00', /* safety green */
-        onyx: '##111111', /* almost black */
+        lemonLime: '#E6FF00' /* safety green */,
+        onyx: '##111111' /* almost black */,
         nero: '#252525' /* dark gray */,
         dolphin: '#7e7f9a' /* purple gray */,
         turquoise: '#40ebd4' /* turquoise */,
@@ -16,7 +16,10 @@ module.exports = {
         tamarillo: '#911412' /* deep red */,
         eerieBlack: '#1e1e1e' /* almost black */,
         jet: '#353535' /* lighter - almost black rollover */,
-      }
+      },
+      dropShadow: {
+        poster: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+      },
     },
     fontFamily: {
       modern: ['Abril Fatface', 'cursive'],
