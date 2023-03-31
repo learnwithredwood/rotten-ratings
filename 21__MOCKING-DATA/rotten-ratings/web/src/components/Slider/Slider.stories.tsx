52bdf075adb37a9d4a863d550b7acb5a5e0777e4
Amy Haywood Dutton <me@amyhaywood.com>
2023-03-31 08:57:09 -0500

    ✨ Finished Lesson 21

--- 21__MOCKING-DATA/rotten-ratings/web/src/components/Slider/Slider.stories.tsx
+++ 21__MOCKING-DATA/rotten-ratings/web/src/components/Slider/Slider.stories.tsx
@@ -2,70 +2,36 @@
 
 import type { ComponentMeta } from '@storybook/react'
 
-import hocusPocus from '../Poster/images/hocus-pocus.jpeg'
-
 import { Slider, type SliderProps } from './Slider'
+import { slides } from './Slider.mocks'
 
 export default {
   title: 'Components/Slider',
   component: Slider,
 } as ComponentMeta<typeof Slider>
 
-const Template = (args) => <Slider {...args} />
+const Template = (args: SliderProps) => <Slider {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
   label: 'Most Popular',
   slideSize: 'small',
   viewAllLink: '/movies',
-  slides: [
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-    {
-      src: hocusPocus,
-      alt: 'Hocus Pocus',
-    },
-  ],
+  slides: slides,
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  label: 'Most Popular',
+  slideSize: 'medium',
+  viewAllLink: '/movies',
+  slides: slides,
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  label: 'Most Popular',
+  slideSize: 'large',
+  viewAllLink: '/movies',
+  slides: slides,
 }
