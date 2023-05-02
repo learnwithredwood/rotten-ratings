c2b3edfbf5c758bb10972bb4f381229fbfdb6c11
Amy Haywood Dutton <me@amyhaywood.com>
2023-05-02 11:31:02 -0500

    ✨ Finished Lesson 29

--- 29__ANIMATE-FEATURE-HERO/rotten-ratings/web/src/components/Feature/Slide/Slide.tsx
+++ 29__ANIMATE-FEATURE-HERO/rotten-ratings/web/src/components/Feature/Slide/Slide.tsx
@@ -1,6 +1,7 @@
+import { motion } from 'framer-motion'
+
 import { IconButton } from 'src/components/IconButton'
 import { Percentage } from 'src/components/Percentage'
-
 interface SlideProps {
   image: string
   percentage: number
@@ -8,6 +9,19 @@ interface SlideProps {
 }
 
 const Slide = ({ image, percentage, title }: SlideProps) => {
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    show: { opacity: 1, y: 0 },
+  }
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { duration: 1.5, staggerChildren: 0.2, delayChildren: 0 },
+    },
+  }
+
   return (
     <div
       className="min-h-[425px] w-full rounded-[40px] bg-cover bg-centerCenter pl-12 pt-12"
@@ -16,17 +30,42 @@ const Slide = ({ image, percentage, title }: SlideProps) => {
       }}
     >
       <div className="flex min-h-[350px] flex-col justify-between">
-        <h2 className="mb-4 max-w-[500px] text-5xl font-bold text-white">
+        <motion.h2
+          variants={variants}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 2, type: 'spring' }}
+          className="mb-4 max-w-[500px] text-5xl font-bold text-white"
+        >
           {title}
-        </h2>
-        <div className="mb-1 flex flex-1 gap-x-3">
-          <IconButton handleClick={() => {}} name="bookmark" />
-          <IconButton handleClick={() => {}} name="watchLater" />
-          <IconButton handleClick={() => {}} name="addList" />
-        </div>
-        <div>
+        </motion.h2>
+        <motion.div
+          className="mb-1 flex flex-1 gap-x-3"
+          variants={container}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <motion.div variants={variants} transition={{ duration: 1 }}>
+            <IconButton handleClick={() => {}} name="bookmark" />
+          </motion.div>
+          <motion.div variants={variants} transition={{ duration: 1 }}>
+            <IconButton handleClick={() => {}} name="watchLater" />
+          </motion.div>
+          <motion.div variants={variants} transition={{ duration: 1 }}>
+            <IconButton handleClick={() => {}} name="addList" />
+          </motion.div>
+        </motion.div>
+        <motion.div
+          variants={variants}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 2, type: 'spring', delay: 0.6 }}
+        >
           <Percentage percentage={percentage} ring={false} size="medium" />
-        </div>
+        </motion.div>
       </div>
     </div>
   )
