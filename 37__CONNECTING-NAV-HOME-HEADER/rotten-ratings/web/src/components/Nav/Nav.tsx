ceb061cbc0d2c3bdbb457680cc0df1ba5c0af92b
Amy Haywood Dutton <me@amyhaywood.com>
2023-05-27 11:03:50 -0500

    ✨ Finished Lesson 37

--- 37__CONNECTING-NAV-HOME-HEADER/rotten-ratings/web/src/components/Nav/Nav.tsx
+++ 37__CONNECTING-NAV-HOME-HEADER/rotten-ratings/web/src/components/Nav/Nav.tsx
@@ -1,18 +1,35 @@
+import { useEffect } from 'react'
+
 import { Icon } from '../Icon/Icon'
 import { Poster } from '../Poster'
 import { Ruler } from '../Ruler'
 import { slides as data } from '../Slider/Slider.mocks'
 
 interface NavProps {
-  // Props
+  handleClick: () => void
 }
 
-const Nav = ({}: NavProps) => {
+const Nav = ({ handleClick }: NavProps) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') handleClick()
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
   return (
     <div className="h-screen w-screen overflow-scroll bg-almostBlack bg-[url('/images/flowers.png')] bg-left-bottom bg-no-repeat">
       <div className="mx-auto grid max-w-pageWidth grid-cols-[minmax(0,_1fr)_minmax(0,_3fr)] gap-x-5 gap-y-8">
         <div className="col-span-2 pt-8">
-          <button className="float-right flex items-center gap-x-3 font-medium uppercase hover:text-lemonLime">
+          <button
+            onClick={handleClick}
+            className="float-right flex items-center gap-x-3 font-medium uppercase hover:text-lemonLime"
+          >
             Close
             <Icon name="close" size={48} />
           </button>
